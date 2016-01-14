ff933191e2e4c920fcd4fa89fc28ab5a520157b3
themaxta <themaxta@gmail.com>
2016-01-14 01:05:55 -0700

    Delivery app, converted arrays to int and float datagit status

--- FileReading/FinalProductWin.c
+++ FileReading/FinalProductWin.c
@@ -9,9 +9,9 @@ int main()
     char str4[200];	// Driver's Name
     char str5[200];	// Delivery Amount
     char str6[200];
-    char cashAmnt[5];
-    char dueAmnt[5];
-    char deliveryAmnt[5];
+    char cashAmnt[7];
+    char dueAmnt[7];
+    char deliveryAmnt[7];
     char driverName[20];
     int check;
     int x = 0;
@@ -136,7 +136,7 @@ int main()
 
     for (int i = 5; i < strlen(str2); i++)      //Starts at five to ignore time data....
     {
-        if (isdigit(str2[i]))
+        if ((isdigit(str2[i])) || (str2[i] == '.'))
         {
 
             cashAmnt[x] = str2[i];
@@ -152,7 +152,7 @@ int main()
     x = 0;
     for (int i = 0; i < strlen(str3); i++)
         {
-            if (isdigit(str3[i]))
+        if ((isdigit(str3[i])) || (str3[i] == '.'))
             {
                 dueAmnt[x] = str3[i];
                 x++;
@@ -201,12 +201,51 @@ int main()
 
     printf("\n\n\nname: %c%c%c%c%c%c%c%c", driverName[0], driverName[1], driverName[2], driverName[3], driverName[4], driverName[5], driverName[6], driverName[7]);
     printf("\ncash: %c%c%c%c", cashAmnt[0], cashAmnt[1], cashAmnt[2], cashAmnt[3]);
-    printf("\ndue: %c%c%c%c", dueAmnt[0], dueAmnt[1], dueAmnt[2], dueAmnt[3]);
+    printf("\ndue: %c%c%c%c%c%c", dueAmnt[0], dueAmnt[1], dueAmnt[2], dueAmnt[3], dueAmnt[4], dueAmnt[5]);
     printf("\nDeliv#: %c%c\n", deliveryAmnt[0], deliveryAmnt[1]);
-    
-    
-    
-    
+
+
+
+
+
+
+
+    // Converting arrays to int and float...
+    int n;
+    sscanf(deliveryAmnt, "%d", &n);
+    printf("\n\n\nDelivery amnt as int variable: %d\n", n);
+
+
+    float c;
+    sscanf(cashAmnt,"%f",&c);
+    printf("Cash amnt var: %.2f\n", c);
+
+    float d;
+    sscanf(dueAmnt, "%f",&d);
+    printf("Due amnt var: %.2f\n", d);
+
+
+
+
+    // Start program ui here:
+    char ansA, ansB, ansC;
+
+    printf("\n\n\n\nWelcome! DriverName!!");
+    printf("\nYou took %d deliveries today! good job!!", n);
+    printf("\nIs this number correct? (Y)/(N) answer plz...\n");
+    scanf(" %c", &ansA);
+    if (ansA == 'N')
+    {
+    	printf("\nPlease enter the correct number.\n");
+    	scanf(" %d", &n);
+    	printf("\nNew delivery number is now: %d", n);
+    }
+
+    printf("\nYou Owe the Pizzeria a total of :  $0.00\n");
+    // Need to convert array into float data.. Fuck
+
+
+
 	return(0);
 	}
 
