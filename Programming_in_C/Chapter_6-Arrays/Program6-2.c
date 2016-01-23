4276c2f661dc438f2814a211d7be417550af83b9
themaxta <themaxta@gmail.com>
2016-01-22 17:28:28 -0700

    chapter 6, arrays

--- Programming_in_C/Chapter_6-Arrays/Program6-2.c
+++ Programming_in_C/Chapter_6-Arrays/Program6-2.c
@@ -1,3 +1,45 @@
 // Program uses an array to input many pieces of data. data is a rating 1 - 10
 
 #include <stdio.h>
+
+int main (void)
+{
+
+	int ratingCount[11], i, response;
+
+	for (i = 1; i <= 10; i++)
+	{
+		ratingCount[i] = 0;					// this puts 0 as the response for every part of array.. except sub [0]
+	}
+
+	printf("Enter your responses\n");
+
+	for (i = 1; i <= 20; i++)
+	{
+		printf("Response # %i:   ", i);
+		scanf("%i", &response);
+
+
+		if ( response < 1 || response > 10 )
+		{
+			printf("Bad response: %i\n", response);
+		}	// end of if
+
+		else
+		{
+			++ratingCounters[response];
+		}
+
+	} // end for
+
+	printf("\n\nRating Number of Responses\n");
+	printf("------ -------------------\n");
+
+	for ( i = 1; i <= 10; i++ )
+	{
+		printf("%4i%4i\n", i, ratingCounters[i]);
+	}
+
+	return (0);
+
+}
--- Programming_in_C/Chapter_6-Arrays/Program6-4.c
+++ Programming_in_C/Chapter_6-Arrays/Program6-4.c
@@ -13,11 +13,11 @@ int main (void)
 	primes[0] = 2;
 	primes[1] = 3;
 
-	for ( p = 5; p <= 50; p += 2)	// starts at 5, bc prime, stps at 50, inc by 2.
+	for ( p = 5; p <= 50; p += 2)	// starts at 5 bc prime, stps at 50, inc by 2.
 	{
 		isPrime = true;
 
-		for ( i = 1; isPrime && p / primes[i] >= primes[i]; ++i) // start at 1, if is prime is true and p(outerloop) / the current prime >= current prime
+		for ( i = 1; isPrime && p / primes[i] >= primes[i]; ++i) // the test checks if the square root of prime has been hit yet... we are done if it is true.
 		{
 			if (p % primes[i] == 0)	// if outer loop / inner loop has no remainder...
 			{
