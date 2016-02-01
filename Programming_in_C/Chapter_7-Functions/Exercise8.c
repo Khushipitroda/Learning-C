dfb9f21f5820e5d89fdfb59ca62c3a74557b0fb1
themaxta <themaxta@gmail.com>
2016-01-31 17:05:43 -0700

    work on chp7 exe

---
+++ Programming_in_C/Chapter_7-Functions/Exercise8.c
@@ -0,0 +1,52 @@
+// Program solves quadratic equations
+// things we need to think about: 
+									/*	1). imaginary numbers
+										2). is passed three numbers
+										3). 1x^2 + 2x + 3 is recipe.
+										4) to find the roots we need to use 
+											x = |-b| square root b^2 - 4(a)(c) / 2 (a) 
+
+
+										*/
+
+
+int quadraticFunc(int a, int b, int c)
+{
+
+	//variables
+	int absoluteB;
+	int temp;
+
+	if (b < 0)
+	{
+		absoluteB = -b;	// Gets abs value.
+	}
+	else
+		absoluteB = b;	// if not negative
+	// first part done.. now the root part..
+
+	// the 4 (a) (c)
+	temp = (b * b) * (4 * (a * c);
+
+	division = a * 2;	
+
+
+}
+
+
+
+int main (void)
+{
+
+	// prototype Declaration
+	int quadraticFunc(int a, int b, int c);
+
+	//variables
+	int result;
+
+	quadraticFunc(3, 5, 10)	// equates to 3x^2 + 5x + 10
+
+
+
+
+}
